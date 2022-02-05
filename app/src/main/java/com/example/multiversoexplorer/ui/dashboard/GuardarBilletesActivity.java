6905cd167d3dfa729fa9df5a12f5ea9d7bbbcadb
curvatia <curvatia2002@yahoo.es>
2022-02-05 13:13:38 +0100

    GuardarBilletesActivity.java

---
+++ app/src/main/java/com/example/multiversoexplorer/ui/dashboard/GuardarBilletesActivity.java
@@ -0,0 +1,16 @@
+package com.example.multiversoexplorer.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multiversoexplorer.R;
+
+public class GuardarBilletesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardar_billetes);
+    }
+}
