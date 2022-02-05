d173427e72638e84f16878a6343d5e8a38edda20
curvatia <curvatia2002@yahoo.es>
2022-02-05 14:25:48 +0100

    Estilos

--- app/src/main/java/com/example/multiversoexplorer/MainActivity.java
+++ app/src/main/java/com/example/multiversoexplorer/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//NEW
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//NEW
         setContentView(R.layout.activity_main);
 
         //OPCION1
